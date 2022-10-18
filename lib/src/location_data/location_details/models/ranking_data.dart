285c74a08a43371b5710e9152cc3b8f6a37e9174
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 04:32:26 +0700

    Create ranking data model
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

--- lib/src/location_data/location_details/models/ranking_data.dart
+++ lib/src/location_data/location_details/models/ranking_data.dart
@@ -1,1 +1,17 @@
-class RankingData {}
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class RankingData {
+  @JsonKey(name: "geo_location_id")
+  final int geoLocationId;
+  @JsonKey(name: "ranking_string")
+  final String rankingString;
+  @JsonKey(name: "geo_location_name")
+  final String geoLocationName;
+  @JsonKey(name: "ranking_out_of")
+  final int rankingOutOf;
+  final int ranking;
+
+  RankingData(this.geoLocationId, this.rankingString, this.geoLocationName,
+      this.rankingOutOf, this.ranking);
+}
