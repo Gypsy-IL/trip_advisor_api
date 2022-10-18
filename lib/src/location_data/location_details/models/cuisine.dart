4f91a37c6b3a12eb6c370a96cf9d13de2de943d6
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 04:16:34 +0700

    Add cousine model
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

--- lib/src/location_data/location_details/models/cuisine.dart
+++ lib/src/location_data/location_details/models/cuisine.dart
@@ -1,1 +1,10 @@
-class Cuisine {}
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Cuisine {
+  final String name;
+  @JsonKey(name: "localized_name")
+  final String localizedName;
+
+  Cuisine(this.name, this.localizedName);
+}
