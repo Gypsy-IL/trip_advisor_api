bca7ae5c6672ffd5b9311262fc8b95ba6c574852
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 02:09:24 +0700

    Create nearby search object
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

---
+++ lib/nearby_search/nearby_search.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:trip_advisor_api/api_settings.dart';
+import 'package:trip_advisor_api/constants/url_constants.dart';
+import 'package:trip_advisor_api/nearby_search/models/nearby_search_parameters.dart';
+import 'package:trip_advisor_api/nearby_search/models/nearby_search_response.dart';
+import 'package:trip_advisor_api/network_utils.dart';
+
+class NearbySearch {
+  final ApiSettings _settings;
+  NearbySearch(this._settings);
+
+  Future<NearbySearchResponse> get(NearbySearchParameters params) async {
+    var uri = Uri.https(UrlConstants.baseUrl,
+        UrlConstants.nearbySearchUnencodedPath, _createQueryParameters(params));
+    var response = await NetworkUtils.getRequest(uri);
+    return NearbySearchResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Map<String, dynamic> _createQueryParameters(NearbySearchParameters params) {
+    Map<String, dynamic> queryParams = {
+      "key": _settings.apiKey,
+      "language": _settings.language.name,
+    };
+    NetworkUtils.addIfNotNull(
+        queryParams, MapEntry("category", params.category));
+    return queryParams;
+  }
+}
