13a94e57aa6714ef09a0f5ba84ab9612373625ee
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 04:23:36 +0700

    Create neighborhood model
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

--- lib/src/location_data/location_details/models/neighborhood.dart
+++ lib/src/location_data/location_details/models/neighborhood.dart
@@ -1,1 +1,10 @@
-class Neighborhood {}
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Neighborhood {
+  @JsonKey(name: "location_id")
+  final String locationId;
+  final String name;
+
+  Neighborhood(this.locationId, this.name);
+}
