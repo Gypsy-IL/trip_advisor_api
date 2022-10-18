832fba888e6abf13445bb06b8d0e9de60ac826cd
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 04:45:08 +0700

    Create location details response model
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

---
+++ lib/src/location_data/location_details/location_details_response.dart
@@ -0,0 +1,11 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:trip_advisor_api/src/location_data/location_details/location_details_result.dart';
+import 'package:trip_advisor_api/trip_advisor_api.dart';
+
+@JsonSerializable()
+class LocationDetailsResponse {
+  final Iterable<LocationDetailsResult>? data;
+  final TripAdvisorError? error;
+
+  LocationDetailsResponse(this.data, this.error);
+}
