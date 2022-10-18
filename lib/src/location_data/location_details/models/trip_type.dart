e38684a59897a5647f4a285bea1f9cf010271daa
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 04:39:26 +0700

    Add trip type model
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

--- lib/src/location_data/location_details/models/trip_type.dart
+++ lib/src/location_data/location_details/models/trip_type.dart
@@ -1,1 +1,11 @@
-class TripType {}
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class TripType {
+  final String name;
+  @JsonKey(name: "localized_name")
+  final String localizedName;
+  final String value;
+
+  TripType(this.name, this.localizedName, this.value);
+}
