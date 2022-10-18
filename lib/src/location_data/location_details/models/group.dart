c3666ef57286f0e0fe56d3cb9bbdc9dd65b15603
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 04:21:26 +0700

    Create group model
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

--- lib/src/location_data/location_details/models/group.dart
+++ lib/src/location_data/location_details/models/group.dart
@@ -1,1 +1,12 @@
-class Group {}
+import 'package:json_annotation/json_annotation.dart';
+import 'package:trip_advisor_api/src/location_data/location_details/models/category.dart';
+
+@JsonSerializable()
+class Group {
+  final String name;
+  @JsonKey(name: "localized_name")
+  final String localizedName;
+  final Iterable<Category> categories;
+
+  Group(this.name, this.localizedName, this.categories);
+}
