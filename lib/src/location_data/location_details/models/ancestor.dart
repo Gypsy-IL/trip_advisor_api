a0c184db1f6e79a99456fc2198c3c3d604e26a26
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 04:12:45 +0700

    Add ancestor model
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

--- lib/src/location_data/location_details/models/ancestor.dart
+++ lib/src/location_data/location_details/models/ancestor.dart
@@ -1,1 +1,16 @@
-class Ancestor {}
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Ancestor {
+  final String? abbrv;
+  final String level;
+  final String name;
+  @JsonKey(name: "location_id")
+  final String locationId;
+
+  Ancestor(
+      {this.abbrv,
+      required this.level,
+      required this.name,
+      required this.locationId});
+}
