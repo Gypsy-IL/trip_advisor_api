70669f22417842e1052e0be7ec61c5255a79bcae
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 04:14:10 +0700

    Create category model
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

--- lib/src/location_data/location_details/models/category.dart
+++ lib/src/location_data/location_details/models/category.dart
@@ -1,1 +1,10 @@
-class Category {}
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Category {
+  final String name;
+  @JsonKey(name: "localized_name")
+  final String localizedName;
+
+  Category(this.name, this.localizedName);
+}
