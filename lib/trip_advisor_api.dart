0275dd16e59bab45b74396f90e3e9ee5aa0e0a18
Roy Lenz <lenz.roy@gmail.com>
2022-10-19 07:52:27 +0700

    Added api class which combines all of the objects
    Signed-off-by: Roy Lenz <lenz.roy@gmail.com>

--- lib/trip_advisor_api.dart
+++ lib/trip_advisor_api.dart
@@ -3,6 +3,13 @@
 /// More dartdocs go here.
 library trip_advisor_api;
 
+import 'package:trip_advisor_api/src/location_data/location_details/location_details.dart';
+import 'package:trip_advisor_api/src/location_data/location_photos/location_photos.dart';
+import 'package:trip_advisor_api/src/location_data/location_reviews/location_reviews.dart';
+import 'package:trip_advisor_api/src/models/api_settings.dart';
+import 'package:trip_advisor_api/src/search/find_search/find_search.dart';
+import 'package:trip_advisor_api/src/search/nearby_search/nearby_search.dart';
+
 export 'src/models/api_settings.dart';
 
 export 'src/search/models/search_response.dart';
@@ -32,3 +39,20 @@ export 'src/location_data/location_reviews/location_review_result.dart';
 export 'src/models/address_object.dart';
 export 'src/models/lat_long.dart';
 export 'src/models/trip_advisor_error.dart';
+
+class TripAdvisorApi {
+  final ApiSettings _settings;
+  late NearbySearch nearbySearch;
+  late FindSearch findSearch;
+  late LocationDetails locationDetails;
+  late LocationPhotos locationPhotos;
+  late LocationReviews locationReviews;
+
+  TripAdvisorApi(this._settings) {
+    nearbySearch = NearbySearch(_settings);
+    findSearch = FindSearch(_settings);
+    locationDetails = LocationDetails(_settings);
+    locationPhotos = LocationPhotos(_settings);
+    locationReviews = LocationReviews(_settings);
+  }
+}
